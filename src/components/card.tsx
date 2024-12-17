e95106b9b5e3ef2f3ec0e2f4c015992a1ea317e1
hozayves <muhozayves2014@gmail.com>
2024-12-17 19:11:14 +0200

    Make a product card touchable

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,15 +1,16 @@
 import { Colors } from "@/constants/Colors";
-import { Image, View, Text, StyleSheet, ImageProps, Pressable } from "react-native";
+import { Image, View, Text, StyleSheet, Pressable } from "react-native";
 
 
-export default function Card({ title, subTitle, image, onPress }: { title: string, subTitle: string, image: ImageProps, onPress: () => void }) {
+export default function Card({ title, subTitle, imageUrl, onPress }: { title: string, subTitle: string, imageUrl: string, onPress: () => void }) {
+
     return (
         <Pressable onPress={onPress}>
             <View style={styles.container}>
                 <Image
                     style={styles.image}
                     resizeMode="cover"
-                    source={image}
+                    source={{ uri: imageUrl }}
                 />
                 <View style={styles.itemsContainer}>
                     <Text style={styles.itemTitle}>{title}</Text>
